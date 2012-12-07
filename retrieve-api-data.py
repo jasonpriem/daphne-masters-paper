7acd9914f33f3b805a521cd75c1b71b851456f5e
Jason Priem <jp@jasonpriem.org>
2012-12-07 03:35:54 -0500

    retrieves data from unc library api for each couchdb item.

--- retrieve-api-data.py
+++ retrieve-api-data.py
@@ -1,9 +1,12 @@
 import couchdb
 import requests
+url_template = "http://search.lib.unc.edu/search?R=[id]&output-format=export&export-option=xml"
 
 couch_server = couchdb.Server(url="http://127.0.0.1:5984")
 db = couch_server["daphne"]
-print db
 
 for row in db.view("main/by-type-and-id"):
-    print row["_id"]
+    query = url_template.replace("[id]", row["id"])
+    doc = row.value
+    res = requests.get(query)
+    doc["api_result"] = res.text

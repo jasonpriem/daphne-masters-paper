eef679cbac51607ce6539058d411eba749ea5ddb
Jason Priem <jp@jasonpriem.org>
2012-12-07 05:56:20 -0500

    bracket notation to do startkey and endkey.

--- retrieve-api-data.py
+++ retrieve-api-data.py
@@ -5,11 +5,15 @@ url_template = "http://search.lib.unc.edu/search?R=[id]&output-format=export&exp
 couch_server = couchdb.Server(url="http://127.0.0.1:5984")
 db = couch_server["daphne"]
 
-for row in db.view("main/by-type-and-id", limit=1, startkey=[0, "zzzzzz"]):
+view_res = db.view("main/has-api-result")
+
+print view_res[[0, False]:[0, "zzzzzz"]]
+
+for row in view_res[[0, False]:[0, "zzzzzz"]]:
     query = url_template.replace("[id]", row["id"])
     doc = row.value
     res = requests.get(query)
     doc["api_result"] = res.text
 
-    print row["id"]
+#    print row["id"]
     db.save(doc)
